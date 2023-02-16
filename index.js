abf00ae0bc57ddd0a24218716af17e97b444df9b
jishenka <jiyeonshenka@gmail.com>
2023-02-16 18:08:18 +0900

    http

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+/////////////////////////////////////// FILE
 
 // Blocking and synchrounous 
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -8,15 +12,37 @@ const fs = require('fs');
 
 
 // Non-blocking and asynchronous
-fs.readFile('./txt/star.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-            console.log(data3);
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('File has been written 😂');
-            });
+// fs.readFile('./txt/star.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('File has been written 😂');
+//             });
+//         });
+//     });
+// });
+// console.log('Will read file!');
+///////////////////////////////////////
+
+////////////////////////////////////// SERVER
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+
+    if(pathName === '/overview') {
+        res.end('overview!');
+    } else if (pathName === '/product') {
+        res.end('Product!');
+    } else {
+        res.writeHead(404, {
+            'Content-type' : 'text/html',
+            'my-own-header': "HHH"
         });
-    });
+        res.end('<h1>Page not found</h1>');
+    }
 });
-console.log('Will read file!');
+
+server.listen(8080, '127.0.0.1', () => {
+    console.log('Listening to req on port 8080.');
+})
